dd989393b3775d4581d15bc5d1cf9164ce8b8fb5
b.pasazade <b.pasazade@outlook.com>
2023-01-18 23:56:52 +0300

    Basri
    checkIllegal, separateToDigits, digitsToString, vowelConsonant

--- Main.java
+++ Main.java
@@ -44,6 +44,59 @@ public class Main {
         }
 
     }
+
+    // Checks every possible substring from index 0 of a prefix by number of vowels and consonants.
+    public static boolean checkVowelConsonant(String password) {
+        int vowelNumber = 0, consonantNumber = 0;
+
+        for (char c : password.toCharArray()) {
+            //Counter for  vowel and consonant
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                vowelNumber++;
+            } else {
+                consonantNumber++;
+            }
+
+            if (vowelNumber > consonantNumber) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    // Gets the password as an integer list then merges them into a string value to return.
+    public static String digitsToString(ArrayList<Integer> digits) {
+        String prefix = "";
+        for (Integer digit : digits) {
+            prefix += numbers.get(digit);
+        }
+        return prefix;
+    }
+
+    // Gets the password and split it into digits as string, then parses them into Integer one by one.
+    public static ArrayList<Integer> seperateToDigits(String string) {
+        String[] digitsStr;
+        digitsStr = string.split("");
+
+        ArrayList<Integer> digitList = new ArrayList<>();
+
+        for (int i = 0; i < digitsStr.length; i++) {
+            digitList.add(Integer.parseInt(digitsStr[i]));
+        }
+
+        return digitList;
+    }
+
+    // Built to use the checkVowelConsonant() function as a shortcut.
+    private static boolean checkIllegal(String password) {
+        if (!checkVowelConsonant(digitsToString(seperateToDigits(password)))) {
+            //System.out.println("Password is illegal!");
+            return true;
+        }
+        return false;
+    }
+
     public static boolean checkOnlyDigits(String password) {
         for (int i = 0; i < password.length(); i++) {
             if (password.charAt(i) < '0' || password.charAt(i) > '9') {
